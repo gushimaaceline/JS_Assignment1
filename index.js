144bcd2331a92135790fa32f7ed1b9e9f4372558
gushimaaceline <celinegushimaa@gmail.com>
2021-03-10 09:11:00 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function fruits() {
+    var fruit_list= ["avocado","grapes","mangoes","oranges","kiwi"];
+    for (let i = 0; i <= fruit_list.length; i++) {
+        console.log(fruit_list[i]);
+    }
+}
+fruits();
