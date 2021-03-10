f35a2a32b54b8a1c41f0ab5341d7e1fbbfe9d568
gushimaaceline <celinegushimaa@gmail.com>
2021-03-10 20:11:54 +0300

    add comments

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function fruits() {
-    var fruit_list= ["avocado","grapes","mangoes","oranges","kiwi"];
-    for (let i = 0; i <= fruit_list.length; i++) {
-        console.log(fruit_list[i]);
+function fruits() {     //declare a function named fruits
+    var fruit_list= ["avocado","grapes","mangoes","oranges","kiwi"]; //stocking the fruits in the array called fruit_list
+    for (let i = 0; i < fruit_list.length; i++) {     //assigning the fruits to indeces
+        console.log(fruit_list[i]);  //print all items fruit_list array
+        
+    } 
+    
     }
-}
-fruits();
+
+fruits();   // call function fruits
